32ae0979337bfab809a22b405141455f92a6b82b
Garik <garik.apreyan@mail.ru>
2018-09-02 20:46:28 +0400

    [rename] FooterDemo to Footer & [fix] bugs in Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,116 +1,77 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-//import { Col, Glyphicon } from 'react-bootstrap';
-import injectSheet from 'react-jss';
-
-import styles from './styles';
-
+import { Col, Container, Row, Footer } from 'mdbreact';
 class FooterPage extends Component {
   render() {
-    const { classes } = this.props;
-    // return (
-    //   <div className={classes.root}>
-    //     <div className={`text-center ${classes.components}`}>
-    //       <Col className={classes.description}>
-    //         <h5 className={`text-uppercase ${classes.title}`}>
-    //           Recipes Advisor
-    //         </h5>
-    //         <p className={`${classes.descriptionBody} ${classes.lightGray}`}>
-    //           Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-    //           Inventore, est, repudiandae? Itaque, asperiores autem beatae nisi
-    //           officia officiis, dolorum minus, enim adipisci magnam unde aperiam
-    //           reprehenderit nobis et aliquid tenetur.
-    //         </p>
-    //       </Col>
-    //       <Col className={classes.support}>
-    //         <h5 className={`text-uppercase ${classes.title}`}>Support</h5>
-    //         <p>
-    //           <Link
-    //             to="/aboutus"
-    //             className={`${classes.lightGray} ${classes.link}`}
-    //           >
-    //             About us
-    //           </Link>
-    //         </p>
-    //         <p>
-    //           <Link
-    //             to="/contactus"
-    //             className={`${classes.lightGray} ${classes.link}`}
-    //           >
-    //             Contact us
-    //           </Link>
-    //         </p>
-    //         <p>
-    //           <Link
-    //             to="/feedback"
-    //             className={`${classes.lightGray} ${classes.link}`}
-    //           >
-    //             Feedback
-    //           </Link>
-    //         </p>
-    //         <p>
-    //           <Link
-    //             to="/technicalquestions"
-    //             className={`${classes.lightGray} ${classes.link}`}
-    //           >
-    //             Technical Questions
-    //           </Link>
-    //         </p>
-    //       </Col>
-    //       <Col className={classes.contact}>
-    //         <h5 className={`text-uppercase ${classes.title}`}>Contact</h5>
-    //         <p className={classes.lightGray}>
-    //           <Glyphicon glyph={'home'} /> Armenia, YVN
-    //         </p>
-    //         <p className={classes.lightGray}>
-    //           <Glyphicon glyph={'envelope'} /> info@gmail.com
-    //         </p>
-    //         <p className={classes.lightGray}>
-    //           <Glyphicon glyph={'phone'} /> + 01 234 567 88
-    //         </p>
-    //       </Col>
-    //     </div>
-    //     <div>
-    //       <hr className={classes.hr} />
-    //       <div className={classes.footerDown}>
-    //         <Col className={classes.copyright}>
-    //           <p>&copy; {new Date().getFullYear()} Copyright:</p>
-    //         </Col>
-    //         <Col>
-    //           <ul className={`list-unstyled list-inline ${classes.downLinks}`}>
-    //             <li className="list-inline-item">
-    //               <a
-    //                 className="btn-floating btn-sm rgba-white-slight mx-1"
-    //                 href="https://mdbootstrap.com/react/5-min-quick-start/"
-    //               >
-    //                 <i className={`fab fa-facebook-square ${classes.icon}`} />
-    //               </a>
-    //             </li>
-    //             <li className="list-inline-item">
-    //               <a className="btn-floating btn-sm rgba-white-slight mx-1">
-    //                 <i className={`fab fa-twitter-square ${classes.icon}`} />
-    //               </a>
-    //             </li>
-    //             <li className="list-inline-item">
-    //               <a className="btn-floating btn-sm rgba-white-slight mx-1">
-    //                 <i
-    //                   className={`fab fa-google-plus-square ${classes.icon}`}
-    //                 />
-    //               </a>
-    //             </li>
-    //             <li className="list-inline-item">
-    //               <a className="btn-floating btn-sm rgba-white-slight mx-1">
-    //                 <i className={`fab fa-linkedin ${classes.icon}`} />
-    //               </a>
-    //             </li>
-    //           </ul>
-    //         </Col>
-    //       </div>
-    //     </div>
-    //   </div>
-    // );
-    return <div />;
+    return (
+      <Footer
+        className="font-small pt-4 mt-4 "
+        style={{ backgroundColor: 'black' }}
+      >
+        <Container>
+          <Row
+            className="text-center text-md-left mt-5 pb-4"
+            style={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <Col md="3" lg="3" xl="3" className="mt-3">
+              <h6 className="text-uppercase mb-4 font-weight-bold light-green-text">
+                Recipes Advisor
+              </h6>
+              <p>
+                Here you can use rows and columns here to organize your footer
+                content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit.
+              </p>
+            </Col>
+            <hr
+              className="w-100 clearfix d-md-none"
+              style={{ marginLeft: '1260px' }}
+            />
+            <Col md="2" lg="2" xl="2" className="mt-3">
+              <h6 className="text-uppercase mb-4 font-weight-bold">Products</h6>
+              <p>
+                <Link to="/aboutus">About Us</Link>
+              </p>
+              <p>
+                <Link to="/contactus">Contact Us</Link>
+              </p>
+            </Col>
+            <hr className="w-100 clearfix d-md-none" />
+            <Col
+              md="4"
+              lg="3"
+              xl="3"
+              className="mt-3"
+              style={{ marginRight: '-35px' }}
+            >
+              <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
+              <p>
+                <i className="fa fa-home mr-3" /> New York, NY 10012, US
+              </p>
+              <p>
+                <i className="fa fa-envelope mr-3" /> info@gmail.com
+              </p>
+              <p>
+                <i className="fa fa-phone mr-3" /> + 01 234 567 88
+              </p>
+              <p>
+                <i className="fa fa-print mr-3" /> + 01 234 567 89
+              </p>
+            </Col>
+          </Row>
+          <hr />
+          <Row>
+            <Col lg="3" style={{ display: 'flex', flexWrap: 'nowrap' }}>
+              <p className="float-xl-right">
+                &copy; {new Date().getFullYear()} Copyright:
+                <a href="https://acafoodapi.haffollc.com"> Recipes Advisor </a>
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </Footer>
+    );
   }
 }
 
-export default injectSheet(styles)(FooterPage);
+export default FooterPage;
