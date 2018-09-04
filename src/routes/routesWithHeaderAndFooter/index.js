d6878d18bdaa8dad25ba65d92208d3353bc26eb8
syuzichachyan <kchachyan@gmail.com>
2018-09-04 15:40:27 +0400

    [delete] unusedimports

--- src/routes/routesWithHeaderAndFooter/index.js
+++ src/routes/routesWithHeaderAndFooter/index.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-
 import Header from '../../containers/Header';
-import Footer from '../../components/Footer';
 import Home from '../../components/Home';
 import Settings from '../../components/Settings';
 import PrivateRoutes from '../../containers/privateRoute';
-import AboutUs from '../../components/AboutUs';
-import ContactUs from '../../components/ContactUs';
 import Recipes from '../../containers/Recipes';
 import RandomRecipes from '../../components/RandomRecipes';
 import Favourites from '../../containers/Favourites';
