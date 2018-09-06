3fd58037350b2417e2d41cb3610f0f56b4a997bb
syuzichachyan <kchachyan@gmail.com>
2018-09-06 13:17:18 +0400

    [add] localStorage.js and [change] configureStore.ja

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,18 +1,42 @@
-import thunk from "redux-thunk";
-import { applyMiddleware, createStore, compose } from "redux";
-import rootReducer from "../reducers";
+import thunk from 'redux-thunk';
+import { applyMiddleware, createStore, compose } from 'redux';
+import rootReducer from '../reducers';
+import { saveState, loadState } from './localStorage';
+import throttle from 'lodash/throttle';
 
+const localStorageStore = loadState('store');
 const store =
-  process.env.NODE_ENV === "production"
-    ? createStore(rootReducer, applyMiddleware(thunk))
+  process.env.NODE_ENV === 'production'
+    ? createStore(rootReducer, localStorageStore, applyMiddleware(thunk))
     : createStore(
         rootReducer,
+        localStorageStore,
         compose(
           applyMiddleware(thunk),
           window.__REDUX_DEVTOOLS_EXTENSION__ &&
             window.__REDUX_DEVTOOLS_EXTENSION__()
         )
       );
- store.subscribe(() => console.log(store.getState()));
+store.subscribe(() => console.log(store.getState()));
+store.subscribe(
+  throttle(() => {
+    saveState(
+      {
+        recipes: store.getState().recipes,
+        isRecipesFetching: store.getState().isRecipesFetching,
+        allFetchFavourites: store.getState().allFetchFavourites,
+        isFavouriteRecipesFetching: store.getState().isFavouriteRecipesFetching,
+        curPage: store.getState().curPage,
+        preferences: store.getState().preferences,
+        user: store.getState().user,
+        form: store.getState().form,
+        filter: store.getState().filter,
+        isPreferencesFetching: store.getState().isPreferencesFetching,
+        jwt: store.getState().jwt
+      },
+      'store'
+    );
+  }, 1000)
+);
 
 export default store;
