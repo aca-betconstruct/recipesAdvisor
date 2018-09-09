9ab8cfb78b5674c6bf4b877c7a06d16ce0475818
tigranuhipoghosyan <poghosyantika@gmail.com>
2018-09-10 00:00:00 +0400

    [change] CommentList

--- src/components/CommentsList/index.js
+++ src/components/CommentsList/index.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import { Container, Row, Col, Input, Button } from 'mdbreact';
 
 class CommentsList extends Component {
-    componentDidMount() {
-    const { getComments,getAuthenticated,jwt } = this.props;
+  componentDidMount() {
+    const { getComments, getAuthenticated, jwt } = this.props;
     getComments();
-    getAuthenticated(jwt)
-
+    getAuthenticated(jwt);
   }
   render() {
-    const {  comments, url, auth } = this.props;
-    console.log(comments);
+    const { comments, url, auth } = this.props;
+    const comment = comments.filter(elem => elem.receptId === url);
     return (
       <div style={{ marginTop: '70px' }}>
         <Row>
@@ -18,27 +17,32 @@ class CommentsList extends Component {
             {!comments.length ? (
               <p>Loading..</p>
             ) : (
-              comments.map((elem, i) => {
-                if (url !== elem.receptId) {
-                  return '';
-                }
+              comment.map((elem, index) => {
+                const user = auth.filter(el => el.id === elem.creatorId);
                 return (
-                  <div className="excerpt" style={{ marginTop: '40px', backgroundColor:"#e6ffe6" }}>
-                    <div className="brief" style={{ backgroundColor:" #ccffcc" }}>
-                      {auth === null ? (
-                        <p>Loading ...</p>
-                      ) : (
-                        auth.map((el, i) => {
-                          if (el.id !== elem.creatorId) {
-                            return '';
-                          }
-                          return (
-                            <p className="font-weight-bold mb-3" key={i+el.firstName}>
-                              {el.firstName} {el.lastName}
-                            </p>
-                          );
-                        })
-                      )}
+                  <div
+                    className="excerpt"
+                    style={{ marginTop: '40px', backgroundColor: '#e6ffe6' }}
+                    key={index}
+                  >
+                    <div
+                      className="brief"
+                      style={{ backgroundColor: ' #ccffcc' }}
+                    >
+                      {auth === null
+                        ? ' '
+                        : !user.length
+                          ? ''
+                          : user.map((el, i) => {
+                              return (
+                                <p
+                                  className="font-weight-bold mb-3"
+                                  key={i + el.firstName}
+                                >
+                                  {el.firstName} {el.lastName}
+                                </p>
+                              );
+                            })}
                     </div>
                     <div>
                       <div className="added-text">{elem.text}</div>
