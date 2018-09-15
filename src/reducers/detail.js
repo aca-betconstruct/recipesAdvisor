d6a09a692b9dac50fea42becad8e1d2284a0c00b
syuzichachyan <kchachyan@gmail.com>
2018-09-15 22:07:01 +0400

    [add] LOGOUT_USER in detail

--- src/reducers/detail.js
+++ src/reducers/detail.js
@@ -1,10 +1,13 @@
-import { RECEIVE_DETAIL } from '../constants';
+import {LOGOUT_USER, RECEIVE_DETAIL} from '../constants';
 
 const detail = (state = null, action) => {
   switch (action.type) {
     case RECEIVE_DETAIL: {
       return action.payload[0];
     }
+      case LOGOUT_USER: {
+          return null;
+      }
     default: {
       return state;
     }
